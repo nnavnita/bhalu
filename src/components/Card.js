c319916aa128591abdaeed51263d866331f9d3d6
nnavnita <navnitanandakumar@gmail.com>
2021-06-05 04:31:09 +1000

    added styles to card view

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import './styles/Card.scss';
 
 const Card = (props) => {
     return( props.text ? <div className = "card">
-         <Panel shaded bordered bodyFill style={{ display: 'inline-block',  }}>
+         <Panel shaded bordered bodyFill style={{ display: 'inline-block' }}>
             <img alt="" src="https://via.placeholder.com/150x150" />
             <Panel header="LISTING">
             <p><small>{props.text}</small></p>
--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
 import AddCard from './AddCard';
-import CardList from './CardList';
+import Card from './Card';
 
 import './styles/ListContainer.scss';
 
 const ListContainer = () => {
-    const [listing, setListing] = useState('');
+    const [listing, setListing] = useState([]);
 
     const getListing = () => {
         return listing;
     }
 
     const AddNewListing = (input) => {
-        setListing(input);
+        setListing(current => [...current, <Card text={input}/>]);
     }
 
-    return(<div className="list_container"><CardList listing={getListing} /><AddCard OkClickHandler={AddNewListing} /></div>);
+    return(<div className="list_container">{getListing()}<AddCard OkClickHandler={AddNewListing} /></div>);
 }
 
 export default ListContainer;
