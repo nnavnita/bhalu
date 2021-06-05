87f8ee51681028ccd33a6c9bfbcfc1072e06b017
nnavnita <navnitanandakumar@gmail.com>
2021-06-05 18:32:23 +1000

    removed buggy theme change functionality

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,52 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import ListContainer from "./ListContainer";
 import { Icon, Toggle } from 'rsuite';
 
 import './styles/Screen.scss';
 
 const Screen = () => {
-  const [theme, setTheme] = useState('screen dark');
-
-  const getTheme = () => {
-    return theme;
-  }
-
-  const handleChange = (value) => {
-    if  (value) {
-      setTheme('screen dark');
-      import('./themes/DarkTheme');
-    } else {
-      setTheme('screen light');
-      import('./themes/LightTheme');
-    }
-  }
-
-//create components using React.lazy
-const LightTheme = React.lazy(() => import('./themes/LightTheme'));
-const DarkTheme = React.lazy(() => import('./themes/DarkTheme'));
-
-//create a parent component that will load the components conditionally using React.Suspense
-const ThemeSelector = ({ children }) => {
-  const CHOSEN_THEME = getTheme() || 'screen dark';
   return (
-    <>
-      <React.Suspense fallback={<></>}>
-        {(CHOSEN_THEME === 'screen light') && <LightTheme />}
-        {(CHOSEN_THEME === 'screen dark') && <DarkTheme />}
-      </React.Suspense>
-      {children}
-    </>
-  )
-}
-
-  return (
-    <div className={getTheme()}>
-      <ThemeSelector>
+    <div className='screen dark'>
         <div className="theme_toggle">
-          <Toggle size="lg" defaultChecked onChange={handleChange.bind(this)} unCheckedChildren={<Icon icon="sun-o" size="2x" />} checkedChildren={<Icon icon="moon-o" size="2x" />} />
+          <Toggle size="lg" defaultChecked unCheckedChildren={<Icon icon="sun-o" size="2x" />} checkedChildren={<Icon icon="moon-o" size="2x" />} />
         </div>
         <ListContainer />
-      </ThemeSelector>
     </div>
   );
 };
--- src/components/themes/DarkTheme.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import 'rsuite/dist/styles/rsuite-dark.css';
-
-const Theme = () => (<React.Fragment></React.Fragment>);
-
-export default Theme;
